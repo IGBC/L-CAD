83fc5c32de4f6d8f5e77e08f80943ddec699bdc6
igbc <fakespamtrap@gmail.com>
2015-08-03 18:21:33 +0100

    Added Initial Logic File

---
+++ gatesim/gate_logic.py
@@ -0,0 +1,14 @@
+class LogicGate:
+	def __init__(self):
+		self.state = False #output state of the gate
+		self.updated = False #
+		self.inputs = [] #gates this gate accepts input from
+		self.outputs = [] #gates this gate outputs to
+
+	#enum gateType 
+	bool state = False
+	bool updated = False
+	def update():
+		pass
+	LogicGate[] children = []
+	LogicGate[] parents = []
