a47cc33a0c9e297c96472d065fb2d7960b035803
IGBC <fakespamtrap@gmail.com>
2016-04-21 14:44:50 +0200

    Started adding GLI handling to Logic gate. A lot of this is boilerplate, I'm thinking maybe this should go into a GLIHandler class that everything can inherit from?

--- L-CAD/src/io/lonelyrobot/igbc/lcad/simulation/gates/LogicGate.java
+++ L-CAD/src/io/lonelyrobot/igbc/lcad/simulation/gates/LogicGate.java
@@ -1,5 +1,32 @@
 package io.lonelyrobot.igbc.lcad.simulation.gates;
 
-public class LogicGate {
+import java.util.List;
 
+import io.lonelyrobot.igbc.lcad.simulation.GenericLogicInterface;
+import io.lonelyrobot.igbc.lcad.simulation.LogicConnection;
+
+public class LogicGate implements GenericLogicInterface {
+
+	private List<LogicConnection> outputList;
+	private List<LogicConnection> inputList;
+	
+	@Override
+	public void connect(GenericLogicInterface outEP) {
+		LogicConnection connection;
+		try {
+			connection = new LogicConnection(this, outEP);
+		}
+		catch (ConnectionfailedException e){
+			//TODO: Log something or something, either way we failed
+			return;
+		}
+		outputList.add(connection);		
+	}
+
+	@Override
+	public void acceptConnection(LogicConnection connection) throws ConnectionfailedException {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
