871b63533cca9e012a96fb1a95a2df2050ebb3e5
IGBC <fakespamtrap@gmail.com>
2016-06-27 23:33:57 +0200

    Began implementing GLI

--- L-CAD/lcadengine/include/lcadengine/GenericLogicInterface.h
+++ L-CAD/lcadengine/include/lcadengine/GenericLogicInterface.h
@@ -1,19 +1,30 @@
 #ifndef GENERICLOGICINTERFACE_H
 #define GENERICLOGICINTERFACE_H
 
-#import "./LogicConnection.h"
+#include <list>
 
+#include "./LogicConnection.h"
+#include "./workers/WorkDispatcherInterface.h"
 class GenericLogicInterface {
 public:
-	GenericLogicInterface() {};
-	~GenericLogicInterface() {};
+    GenericLogicInterface(int maxInputs, WorkDispatcherInterface &dispatcher);
+    ~GenericLogicInterface();
 
-	virtual void update();
-	virtual void isState();
-	virtual void connect(LogicConnection connection);
-	virtual void acceptConnection(LogicConnection connection) throws ConnectionfailedException;
-	virtual void destroyConnection(LogicConnection connection);
+    void update();
+    void isState();
+    void connect(LogicConnection &connection);
+    void acceptConnection(LogicConnection &connection); // throws ConnectionfailedException;
+    void destroyConnection(LogicConnection &connection);
 
+protected:
+    
+    std::list<&LogicConnection> inputList;
+    std::list<&LogicConnection> outputList;
+    
+    const int maxInputs;
+    const WorkDispatcherInterface &dispatcher;
+    int propDelay; // Propergation delay of the gate
+    
 };
 
 #endif // GENERICLOGICINTERFACE_H
