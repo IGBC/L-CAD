9d1e42b1c7b2694e2ca7447d73a3a7304679ee4b
SEGFAULT <fakespamtrap@gmail.com>
2016-07-01 00:08:43 +0200

    Started adding API

--- L-CAD/lcadengine/include/lcadengine/logicTable.h
+++ L-CAD/lcadengine/include/lcadengine/logicTable.h
@@ -18,5 +18,18 @@ struct {
 } typedef connection;
 
 struct context {
-	
+	hashmap *IDMap, *inEpMap, *outEpMap;
+	hashmap *connMap;
 }	
+
+void create_context();
+void delete_context();
+
+uint64_t add_gli();
+void remove_gli(uint64_t ID);
+genricLogicInterface get_gli(uint64_t ID);
+
+uint64_t add_conn();
+void remove_conn(uint64_t ID);
+//TODO: Work out how to get connections.
+
