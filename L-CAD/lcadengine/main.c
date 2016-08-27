3d52ef844f1b36a24f7ca3eb4552303f4ba1a471
IGBC <fakespamtrap@gmail.com>
2016-08-27 11:39:14 +0200

    started test program

---
+++ L-CAD/lcadengine/main.c
@@ -0,0 +1,11 @@
+#include <logicGraph.h>
+#include <dispatcher.h>
+
+int main() {
+    // populate graph with one nor gate.
+    graph *g = create_graph();
+    unsigned long ID = add_gli(g, OR, true, 0);
+    
+    dispatcher *d = create_dispatcher(g, 4);
+    
+}
