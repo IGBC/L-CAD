fc9c4195dcffd98dd5103f544eccecc9d78a8d74
IGBC <fakespamtrap@gmail.com>
2016-08-29 00:11:05 +0200

    extended main program, revealing the engine doesn't work

--- L-CAD/lcadengine/src/main.c
+++ L-CAD/lcadengine/src/main.c
@@ -5,10 +5,26 @@
 int main() {
     // populate graph with one nor gate.
     graph *g = create_graph();
-    unsigned long ID = add_gli(g, OR, true, 0);
+    unsigned long in1 = add_gli(g, UNITY, false, 0);
+    unsigned long in2 = add_gli(g, UNITY, false, 0);
+    unsigned long ID = add_gli(g, AND, true, 0);
+    add_conn(g, in1, ID);
+    add_conn(g, in2, ID);    
+    get_gli(g, ID)->state = true;
     dispatcher *d = create_dispatcher(g, 4);
+    printf("output values are: %i, %i, %i\n",(int *) get_gli(g, in1)->state, (int *) get_gli(g, in2)->state, (int *) get_gli(g, ID)->state);
     dispatcher_add_job(d, ID, 1);
     step_dispatcher(d);
+    printf("output values are: %i, %i, %i\n",(int *) get_gli(g, in1)->state, (int *) get_gli(g, in2)->state, (int *) get_gli(g, ID)->state);
+    get_gli(g, in2)->state = true;
+    dispatcher_add_job(d, ID, 1);
+    step_dispatcher(d);
+    printf("output values are: %i, %i, %i\n",(int *) get_gli(g, in1)->state, (int *) get_gli(g, in2)->state, (int *) get_gli(g, ID)->state);
+    step_dispatcher(d);
+    printf("output values are: %i, %i, %i\n",(int *) get_gli(g, in1)->state, (int *) get_gli(g, in2)->state, (int *) get_gli(g, ID)->state);
+    step_dispatcher(d);
+    printf("output values are: %i, %i, %i\n",(int *) get_gli(g, in1)->state, (int *) get_gli(g, in2)->state, (int *) get_gli(g, ID)->state);
+    
     delete_dispatcher(d);
     delete_graph(g);
     return 0;    
