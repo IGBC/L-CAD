97efb85e072e2d3a094fcb3dafc07c170cad9d5f
Katharina Sabel <sabel.katharina@gmail.com>
2016-08-29 01:46:09 +0200

    Making truth-table demo. It's all a lie!

--- L-CAD/lcadengine/src/main.c
+++ L-CAD/lcadengine/src/main.c
@@ -7,24 +7,86 @@ int main() {
     graph *g = create_graph();
     unsigned long in1 = add_gli(g, UNITY, false, 0);
     unsigned long in2 = add_gli(g, UNITY, false, 0);
-    unsigned long ID = add_gli(g, AND, true, 0);
+    unsigned long in3 = add_gli(g, UNITY, false, 0);
+    unsigned long ID = add_gli(g, RAND, false, 0);
     add_conn(g, in1, ID);
-    add_conn(g, in2, ID);    
-    get_gli(g, ID)->state = true;
+    add_conn(g, in2, ID);
+    add_conn(g, in3, ID);
+    //get_gli(g, ID)->state = true;
     dispatcher *d = create_dispatcher(g, 4);
-    printf("output values are: %i, %i, %i\n",(int *) get_gli(g, in1)->state, (int *) get_gli(g, in2)->state, (int *) get_gli(g, ID)->state);
+    //printf("output values are: %i, %i, %i\n",(int *) get_gli(g, in1)->state, (int *) get_gli(g, in2)->state, (int *) get_gli(g, ID)->state);
+
+
+    get_gli(g, in1)->state = false;
+    get_gli(g, in2)->state = false;
+    get_gli(g, in3)->state = false;
     dispatcher_add_job(d, ID, 1);
     step_dispatcher(d);
-    printf("output values are: %i, %i, %i\n",(int *) get_gli(g, in1)->state, (int *) get_gli(g, in2)->state, (int *) get_gli(g, ID)->state);
+    printf("output values are: %i, %i, %i = %i\n",(int *) get_gli(g, in1)->state, (int *) get_gli(g, in2)->state, (int *) get_gli(g, in3)->state, (int *) get_gli(g, ID)->state);
+
+    get_gli(g, in1)->state = false;
+    get_gli(g, in2)->state = false;
+    get_gli(g, in3)->state = true;
+    dispatcher_add_job(d, ID, 1);
+    step_dispatcher(d);
+    printf("output values are: %i, %i, %i = %i\n",(int *) get_gli(g, in1)->state, (int *) get_gli(g, in2)->state, (int *) get_gli(g, in3)->state, (int *) get_gli(g, ID)->state);
+
+
+
+    get_gli(g, in1)->state = false;
     get_gli(g, in2)->state = true;
+    get_gli(g, in3)->state = false;
     dispatcher_add_job(d, ID, 1);
     step_dispatcher(d);
-    printf("output values are: %i, %i, %i\n",(int *) get_gli(g, in1)->state, (int *) get_gli(g, in2)->state, (int *) get_gli(g, ID)->state);
+    printf("output values are: %i, %i, %i = %i\n",(int *) get_gli(g, in1)->state, (int *) get_gli(g, in2)->state, (int *) get_gli(g, in3)->state, (int *) get_gli(g, ID)->state);
+
+
+
+    get_gli(g, in1)->state = false;
+    get_gli(g, in2)->state = true;
+    get_gli(g, in3)->state = true;
+    dispatcher_add_job(d, ID, 1);
     step_dispatcher(d);
-    printf("output values are: %i, %i, %i\n",(int *) get_gli(g, in1)->state, (int *) get_gli(g, in2)->state, (int *) get_gli(g, ID)->state);
+    printf("output values are: %i, %i, %i = %i\n",(int *) get_gli(g, in1)->state, (int *) get_gli(g, in2)->state, (int *) get_gli(g, in3)->state, (int *) get_gli(g, ID)->state);
+
+
+
+    get_gli(g, in1)->state = true;
+    get_gli(g, in2)->state = false;
+    get_gli(g, in3)->state = false;
+    dispatcher_add_job(d, ID, 1);
     step_dispatcher(d);
-    printf("output values are: %i, %i, %i\n",(int *) get_gli(g, in1)->state, (int *) get_gli(g, in2)->state, (int *) get_gli(g, ID)->state);
-    
+    printf("output values are: %i, %i, %i = %i\n",(int *) get_gli(g, in1)->state, (int *) get_gli(g, in2)->state, (int *) get_gli(g, in3)->state, (int *) get_gli(g, ID)->state);
+
+
+
+    get_gli(g, in1)->state = true;
+    get_gli(g, in2)->state = false;
+    get_gli(g, in3)->state = true;
+    dispatcher_add_job(d, ID, 1);
+    step_dispatcher(d);
+    printf("output values are: %i, %i, %i = %i\n",(int *) get_gli(g, in1)->state, (int *) get_gli(g, in2)->state, (int *) get_gli(g, in3)->state, (int *) get_gli(g, ID)->state);
+
+
+
+    get_gli(g, in1)->state = true;
+    get_gli(g, in2)->state = true;
+    get_gli(g, in3)->state = false;
+    dispatcher_add_job(d, ID, 1);
+    step_dispatcher(d);
+    printf("output values are: %i, %i, %i = %i\n",(int *) get_gli(g, in1)->state, (int *) get_gli(g, in2)->state, (int *) get_gli(g, in3)->state, (int *) get_gli(g, ID)->state);
+
+
+
+    get_gli(g, in1)->state = true;
+    get_gli(g, in2)->state = true;
+    get_gli(g, in3)->state = true;
+    dispatcher_add_job(d, ID, 1);
+    step_dispatcher(d);
+    printf("output values are: %i, %i, %i = %i\n",(int *) get_gli(g, in1)->state, (int *) get_gli(g, in2)->state, (int *) get_gli(g, in3)->state, (int *) get_gli(g, ID)->state);
+
+
+
     delete_dispatcher(d);
     delete_graph(g);
     return 0;    
