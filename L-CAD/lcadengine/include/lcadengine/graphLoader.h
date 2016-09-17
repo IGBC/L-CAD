32cb3b0928bfd847cb4b3da4e3e3701d010efdea
SEGFAULT <fakespamtrap@gmail.com>
2016-09-17 13:34:38 +0200

    commented graphLoader.h

--- L-CAD/lcadengine/include/lcadengine/graphLoader.h
+++ L-CAD/lcadengine/include/lcadengine/graphLoader.h
@@ -3,6 +3,18 @@
 
 #include <lcadengine/logicGraph.h>
 
+/**
+ * @brief Creates a graph from the given string.
+ *
+ * Creates a new logicGraph from a provided graph description string.
+ * This function is intended to be used with a string read from a file.
+ *
+ * @param char *str String to convert to graph. String is unmodified by
+ * 					the conversion process.
+ *
+ * @return logicGraph Graph described by the input string.
+ * 					  NULL on error.
+ */
 graph *loaderLoad(char *str);
 
 #endif
