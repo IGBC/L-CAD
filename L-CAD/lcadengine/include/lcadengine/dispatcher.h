ae6077a3cf9567e151063a91fb27d4996af3e2ba
SEGFAULT <fakespamtrap@gmail.com>
2016-09-17 14:04:47 +0200

    commented dispatcher.h

--- L-CAD/lcadengine/include/lcadengine/dispatcher.h
+++ L-CAD/lcadengine/include/lcadengine/dispatcher.h
@@ -6,9 +6,58 @@
 struct s_dispatcher;
 typedef struct s_dispatcher dispatcher;
 
+/**
+ * @brief Creates a new dispatcher ready to work on the provided graph.
+ *
+ * Creates a new dispatcher with an empty job queue ready to act on the graph
+ * provided as an argument. This Graph is modified by this action and should
+ * only be used with one dispatcher at a time.
+ *
+ * Note: Crashes on error
+ *
+ * @param graph *logicGraph   graph to use with the new dispatcher.
+ * @param int   threads       number of worker threads to use.
+ *
+ * @return dispatcher*  Newly created dispatcher.
+ */
 dispatcher *dispatcherCreate(graph *logicGraph, int threads);
+
+/**
+ * @brief Safely Removes dispatcher
+ *
+ * Deletes dispatcher specified and clears all allocated memory.
+ * Leaves internal logicGraph valid and in a safe state.
+ *
+ * @param dispatcher* Dispatcher to destroy.
+ */
 void dispatcherDelete(dispatcher *ctx);
+
+/**
+ * @brief Steps dispatcher simulation forward by one step.
+ *
+ * Steps dispatcher simulation forward one step, processing all existing jobs
+ * for the current time step and generating future jobs.
+ *
+ * @param  dispatcher*   dispatcher to step.
+ * @return int    		 0 (literally it just returns 0)
+ */
 int dispatcherStep(dispatcher *ctx);
+
+/**
+ * @brief Manually submit a job to the dispatcher (good for inputs)
+ *
+ * Submit a job for the dispatcher to process in $delay steps from now
+ * (now is relative to the dispatcher's internal state). Gate updated is $ID
+ *
+ * This is particularly useful for updating graph inputs. first set the input's
+ * state in the graph then use this command to tell the dispatcher it changed.
+ *
+ * @param dispatcher     *ctx   dispatcher to use
+ * @param unsigned long  ID     ID of gate to update
+ * @param unsigned int   delay  how many steps from now to update (min 1)
+ *
+ * @return int  0 (literally it just returns 0)
+ */
 int dispatcherAddJob(dispatcher *ctx, unsigned long ID, unsigned int delay);
 
 
